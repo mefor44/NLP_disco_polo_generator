640f216e6c5d7e76b4a8e13d5980627547f00d95
PaulinaPacyna <pacyna1998@gmail.com>
2022-03-26 14:46:38 +0100

    moved

--- src/pickle_to_txt.py
+++ src/pickle_to_txt.py
@@ -7,8 +7,8 @@ def clean(txt):
     return bytes(re.sub('\n+', '\n', re.sub(r"(Ref.+|[0-9]+\.|\\r|\r|(^ ))", '', newlines)), 'utf-8').decode('utf-8', 'ignore')
 
 
-df = pd.read_pickle('data/songs_akcent')
+df = pd.read_pickle('../data/songs_akcent')
 cleansed = [clean(txt) for txt in df]
-with open('data/songs_akcent.txt', 'w', encoding='utf-8') as f:
+with open('../data/songs_akcent.txt', 'w', encoding='utf-8') as f:
     f.write(u'\n\n'.join(cleansed))
 print(cleansed[0])
